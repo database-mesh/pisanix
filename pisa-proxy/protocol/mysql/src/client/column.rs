e5d5ebd7d1c74a9483b419a76d270e0584db0c8a
xuanyuan300 <xuanyuan300@gmail.com>
2022-06-10 19:06:04 +0800

    chore(protocol): fix license
    Signed-off-by: xuanyuan300 <xuanyuan300@gmail.com>

--- pisa-proxy/protocol/mysql/src/client/column.rs
+++ pisa-proxy/protocol/mysql/src/client/column.rs
@@ -1,19 +1,20 @@
-// Copyright 2022 mworks
-// 
+// Copyright 2022 SphereEx Authors
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use bytes::{ BytesMut, Buf };
-use crate::{util::BufExt, mysql_const::ColumnType};
+use bytes::{Buf, BytesMut};
+
+use crate::{mysql_const::ColumnType, util::BufExt};
 
 #[derive(Debug)]
 pub struct ColumnInfo {
@@ -34,7 +35,7 @@ pub trait ColumnBuf: BufExt {
 /// Implments ColumnBuf for BytesMut.
 /// For example:
 /// let mut buf = BytesMut::new(&[0x01,0x02]);
-/// buf.parse_column_info(); 
+/// buf.parse_column_info();
 impl ColumnBuf for BytesMut {
     // Parse column info, see https://dev.mysql.com/doc/internals/en/com-query-response.html#packet-Protocol::ColumnDefinition41
     fn parse_column_info(&mut self) -> ColumnInfo {
@@ -51,7 +52,7 @@ impl ColumnBuf for BytesMut {
         if !is_null && !(str_bytes.len() == 1 && str_bytes[0] == 0) {
             table_name = Some(String::from_utf8(str_bytes).unwrap());
         }
-        
+
         //Org table -- physical table-name
         self.get_lenc_str_bytes();
 
@@ -81,13 +82,13 @@ impl ColumnBuf for BytesMut {
         //decimals -- max shown decimal digits
         let decimals = self.get_u8();
 
-        ColumnInfo { 
-            table_name, 
+        ColumnInfo {
+            table_name,
             column_name,
             charset,
-            column_length, 
-            column_type, 
-            column_flag, 
+            column_length,
+            column_type,
+            column_flag,
             decimals,
         }
     }
@@ -96,13 +97,14 @@ impl ColumnBuf for BytesMut {
 #[cfg(test)]
 mod test {
     use bytes::BytesMut;
-    use crate::{mysql_const::ColumnType, client::column::ColumnBuf};
+
+    use crate::{client::column::ColumnBuf, mysql_const::ColumnType};
 
     #[test]
     fn test_parse_column_info() {
         let data = [
-            0x03, 0x64, 0x65, 0x66, 0x00, 0x00, 0x00, 0x01, 0x3f, 0x00, 0x0c,
-            0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfd, 0x80, 0x00, 0x00, 0x00, 0x00,
+            0x03, 0x64, 0x65, 0x66, 0x00, 0x00, 0x00, 0x01, 0x3f, 0x00, 0x0c, 0x3f, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0xfd, 0x80, 0x00, 0x00, 0x00, 0x00,
         ];
 
         let mut buf = BytesMut::from(&data[..]);
@@ -113,4 +115,3 @@ mod test {
         assert_eq!(info.column_length, 0);
     }
 }
-
