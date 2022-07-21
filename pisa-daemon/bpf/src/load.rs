50a71200f2ee56e9626945dae3c19040a7048dc4
xuanyuan300 <xuanyuan300@gmail.com>
2022-07-21 16:15:02 +0800

    feat(pisa-daemon): WIP init pisa-daemon Signed-off-by: xuanyuan300 <xuanyuan300@gmail.com>

---
+++ pisa-daemon/bpf/src/load.rs
@@ -0,0 +1,135 @@
+// Copyright 2022 SphereEx Authors
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::convert::{TryFrom, TryInto};
+use std::io::{ErrorKind, Error};
+use std::os::unix::io::AsRawFd;
+use std::path::Path;
+use aya::programs::{SocketFilter, tc, SchedClassifier, TcAttachType};
+use aya::maps::{ProgramArray, HashMap, MapRefMut};
+use aya::{Bpf, Pod, BpfLoader};
+
+use socket2::{Socket, Domain, Type, Protocol};
+
+pub enum LoadSockFilter {
+    App,
+    SQL,
+}
+
+impl LoadSockFilter {
+    fn open_device(device: String) -> Result<Socket, Box<dyn std::error::Error>> {
+        let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?;
+        socket.bind_device(Some(device.as_bytes()))?;
+        Ok(socket)
+    }
+
+    fn load_app_filter<P: AsRef<Path>>(&self, device: String, path: P) -> Result<(), Box<dyn std::error::Error>> {
+        let mut bpf = Bpf::load_file(path)?;
+        let prog: &mut SocketFilter = bpf.program_mut("app").unwrap().try_into()?;
+        prog.load()?;
+
+        let socket = Self::open_device(device)?;
+        prog.attach(socket.as_raw_fd())?;
+
+        //let mut app_endpoints = HashMap::try_from(bpf.map_mut("app_endpoints")?)?;
+
+        Ok(())
+    }
+
+}
+
+
+#[derive(Debug, Clone, Copy)]
+struct Endpoint {
+	ip: u32,
+    port: u16,
+}
+
+// Implements `Pod` to `Endpoint` as Map Key.
+unsafe impl Pod for Endpoint { }
+
+pub enum TrafficTyp {
+    App,
+    SQL,
+}
+
+pub const APP_ENDPOINTS_CLASSID_PIN_PATH: &str = "/sys/fs/bpf/pisa-daemon/app-endpoints-classid";
+pub const APP_ENDPOINTS_CLASSID_MAP_NAME: &str = "app_endpoints_classid";
+
+impl TrafficTyp {
+    fn add_clsact(&self, device: &str) -> Result<(), Box<dyn std::error::Error>> {
+        if let Err(e) = tc::qdisc_add_clsact(&device) {
+            if e.kind() != ErrorKind::AlreadyExists {
+                return Err(Box::new(e))
+            }
+        }
+        Ok(())
+    }
+
+    pub fn load<P: AsRef<Path>>(&self, path: P, device: &str) -> Result<Bpf, Box<dyn std::error::Error>> {
+        self.add_clsact(device)?;
+        match self {
+            Self::App => {
+                self.app(path, device)
+            },
+
+            Self::SQL => todo!()
+        }
+    }
+
+    fn app<P: AsRef<Path>>(&self, path: P, device: &str) ->  Result<Bpf, Box<dyn std::error::Error>> {
+        if !Path::new(APP_ENDPOINTS_CLASSID_PIN_PATH).exists() {
+            let _ = std::fs::create_dir_all(APP_ENDPOINTS_CLASSID_PIN_PATH);
+        }
+
+        let mut bpf = BpfLoader::new().map_pin_path(APP_ENDPOINTS_CLASSID_PIN_PATH).load_file(path)?;
+        let prog: &mut SchedClassifier = bpf.program_mut("classifier").unwrap().try_into()?;
+        
+        prog.load()?;
+        prog.attach(&device, TcAttachType::Egress)?;
+        Ok(bpf)
+    }
+
+    // Todo, need to add config parameter
+    pub fn load_app_config(&self, bpf: &mut Bpf) -> Result<MapRefMut, Box<(dyn std::error::Error + 'static)>> {
+        bpf.map_mut(APP_ENDPOINTS_CLASSID_MAP_NAME).map_err(|e| e.into())
+        //map.insert(Endpoint{ ip: 1, port: 1 }, 1, 0)?;
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::process::Command;
+
+    use aya::maps::HashMap;
+
+    use crate::load::{Endpoint, TrafficTyp};
+
+    #[test]
+    fn test_load_app_config() {
+        let _ = Command::new("clang").args("-O2 -target bpf -g -c tc/app.c -o tc/app.o -I ./".split(" ")).spawn();
+        let load = TrafficTyp::App;
+        load.add_clsact("lo");
+        let try_bpf = load.app("tc/app.o", "lo");
+        assert_eq!(try_bpf.is_err(), false);
+        let bpf = try_bpf.unwrap();
+        let mut map = HashMap::<_, Endpoint, u32>::try_from(bpf.map_mut("app_endpoints_classid").unwrap()).unwrap();
+        
+        let ep = Endpoint { ip: 11111, port: 8000 };
+        map.insert(ep, 1, 0).unwrap();
+
+        let v = map.get(&ep, 0).unwrap();
+        assert_eq!(v, 1);
+    }
+}
---
+++ pisa-daemon/tc/src/tc.rs
@@ -0,0 +1,300 @@
+// Copyright 2022 SphereEx Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::process::Command;
+
+#[derive(Clone)]
+pub struct QdiscRootAttr<'a> {
+    netns: Option<&'a str>,
+    device: &'a str,
+    typ: &'a str,
+}
+
+pub fn add_root_qdisc<'a>(attr: &QdiscRootAttr<'a>) -> bool {
+    let mut args = vec![
+        "qdisc",
+        "add",
+        "dev",
+        &attr.device,
+        "root",
+        "handle",
+        "1:",
+        &attr.typ,
+    ];
+
+    execute_tc_command!( attr, args, "add root qdisc")
+
+}
+
+pub fn delete_root_qdisc<'a>(device: &str) -> bool {
+    let args = vec![
+        "qdisc",
+        "delete",
+        "dev",
+        device,
+        "root",
+        "handle",
+        "1:",
+    ];
+
+    let out = Command::new("tc")
+        .args(&args)
+        .output()
+        .expect("faild to delete root qdisc");
+
+
+    if !out.status.success() {
+        println!("Failed to delete root qdisc to device {}: {:?}", device, out);
+        return false;
+    }
+
+    true
+}
+
+pub fn show_qdisc(device: String) -> bool {
+    let out = Command::new("tc")
+        .args(["qdisc", "show", "dev", &device])
+        .output()
+        .expect("faild to show qdisc");
+
+    if !out.status.success() {
+        println!("Failed to show qdisc to device {}: {:?}", device, out);
+
+        return false;
+    }
+
+    true
+}
+
+pub struct ClassAttr<'a> {
+    netns: Option<&'a str>,
+    device: &'a str,
+    parent: Option<&'a str>,
+    class_id: &'a str,
+    rate: &'a str,
+    ceil: Option<&'a str>,
+}
+
+pub fn add_class<'a>(attr: &ClassAttr<'a>) -> bool {
+    let mut args = vec![
+        "class",
+        "add",
+        "dev",
+        &attr.device,
+        "parent",
+        &attr.parent.unwrap_or("1:"),
+        "classid",
+        &attr.class_id,
+        "htb",
+        "rate",
+        &attr.rate,
+    ];
+
+    if let Some(ceil) = &attr.ceil {
+        args.push("ceil");
+        args.push(ceil);
+    }
+
+
+    execute_tc_command!( attr, args, "add class")
+
+}
+
+pub fn delete_class<'a>(attr: &ClassAttr<'a>) -> bool {
+    let mut args = vec![
+        "class",
+        "delete",
+        "dev",
+        &attr.device,
+        "parent",
+        "1:",
+        "classid",
+        &attr.class_id,
+        "htb",
+        "rate",
+        &attr.rate,
+    ];
+
+
+    if let Some(ceil) = &attr.ceil {
+        args.push("ceil");
+        args.push(ceil);
+    }
+
+
+    execute_tc_command!( attr, args, "delete class")
+
+}
+
+pub fn show_class(netns: Option<&str>, device: &str) -> (Vec<ClassResult>, bool) {
+    let mut args = vec!["class", "show", "dev", &device];
+
+    if let Some(ns) = netns {
+        args.insert(0, "-n");
+        args.insert(1, ns);
+    }
+    
+    let out = Command::new("tc")
+        .args(&args)
+        .output()
+        .expect("faild to show class");
+
+    if !out.status.success() {
+        println!("Failed to show class to device {}: {:?}", device, out);
+
+        return (vec![], false);
+    }
+
+   let lines  = std::str::from_utf8(&out.stdout).unwrap().lines();
+
+   let mut res = vec![];
+   for line in lines {
+        let arr = line.split(" ").map(|x| x.to_string()).collect::<Vec<_>>();
+       res.push(ClassResult::from(arr));
+
+   }
+
+    (res, true)
+}
+
+#[derive(Debug)]
+pub enum ClassResult {
+    Htb(Htb)
+}
+
+#[derive(Debug)]
+pub struct Htb {
+    pub parent: String,
+    pub class_id: String,
+    pub rate: String,
+    pub ceil: String,
+}
+
+impl  From<&[String]> for Htb {
+    fn from(input: &[String]) -> Self {
+        let parent = if &input[1] == "root" {
+            "root"
+        } else {
+           &input[2]
+        };
+
+        let rate_idx = input.iter().position(|x| x == "rate").unwrap();
+        let ceil_idx = input.iter().position(|x| x == "ceil").unwrap();
+
+        Htb { 
+            parent: parent.to_string(), 
+            class_id: input[0].clone(), 
+            rate: input[rate_idx+1].clone(), 
+            ceil: input[ceil_idx+1].clone(),
+        }
+    }
+}
+
+impl From<Vec<String>> for ClassResult {
+    fn from(input: Vec<String>) -> ClassResult {
+        match input[1].as_str() {
+           "htb" => {
+                ClassResult::Htb(Htb::from(&input[2..]))
+           },
+           _ => unreachable!()
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    
+    fn create_test_ns(ns: &str) {
+        let out = Command::new("ip")
+            .args(["netns", "add", ns])
+            .output()
+            .unwrap();
+        println!("creat test ns {:?}", out);
+    }
+
+    fn delete_test_ns(ns: &str) {
+        let out = Command::new("ip")
+            .args(["netns", "delete", ns])
+            .output()
+            .unwrap();
+        println!("delete test ns {:?}", out);
+    }
+
+
+    fn create_root_qdisc(ns: &str) -> bool {
+        let attr = QdiscRootAttr {
+            netns: Some(ns),
+            device: "lo",
+            typ: "htb",
+        };
+
+        add_root_qdisc(&attr)
+    }
+
+    #[test]
+    fn test_create_root_qdisc() {
+        create_test_ns("footest1");
+        assert_eq!(create_root_qdisc("footest1"), true);
+        delete_test_ns("footest1");
+    }
+
+    #[test]
+    fn test_create_delete_class() {
+        create_test_ns("footest2");
+
+        create_root_qdisc("footest2");
+
+        let attr = ClassAttr {
+            netns: Some("footest2"),
+            device: "lo",
+            class_id: "1:10",
+            rate: "1mbps",
+            ceil: None,
+            parent: None,
+        };
+
+        assert_eq!(add_class(&attr), true);
+        assert_eq!(delete_class(&attr), true);
+
+        delete_test_ns("footest2");
+    }
+
+    #[test]
+    fn test_show_class() {
+        create_test_ns("footest");
+        create_root_qdisc("footest");
+
+        let attr = ClassAttr {
+            netns: Some("footest"),
+            device: "lo",
+            class_id: "1:10",
+            rate: "1mbps",
+            ceil: None,
+            parent: Some("1:1"),
+        };
+
+        add_class(&attr);
+
+        let (res, _ ) = show_class(Some("footest"), "lo");
+
+        let ClassResult::Htb(htb) = &res[0];
+        {
+            assert_eq!(htb.class_id, "1:10");
+        }
+        
+        delete_test_ns("footest");
+    }
+}
