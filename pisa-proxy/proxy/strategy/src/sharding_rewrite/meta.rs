e1957764aa1bec983413000f57d0e9a8b6d55cf6
Jack <875818649@qq.com>
2022-09-27 18:09:00 +0800

    chore(sharding): add DeleteStmt (#344)
    Signed-off-by: wbtlb <wangbo@sphere-ex.com>
    
    Signed-off-by: wbtlb <wangbo@sphere-ex.com>
    Co-authored-by: wbtlb <wangbo@sphere-ex.com>

--- pisa-proxy/proxy/strategy/src/sharding_rewrite/meta.rs
+++ pisa-proxy/proxy/strategy/src/sharding_rewrite/meta.rs
@@ -211,6 +211,12 @@ impl Transformer for RewriteMetaData {
                 self.push_table(t.table_name.clone());
             }
 
+            Node::DeleteStmt(t) => {
+                if let Some(table) = &t.table_name {
+                    self.push_table(table.clone());
+                }
+            }
+
             Node::FromClause(_) => {
                 self.state = ScanState::TableName;
             }
@@ -366,7 +372,6 @@ impl Transformer for RewriteMetaData {
                 Expr::LiteralExpr(Value::Num { span, value, signed }) => {
                     match &mut self.state {
                         ScanState::Where(args) => {
-                            //println!("args {:?}", args);
                             if args.len() > 1 && args[0] == "=" {
                                 let right_value = if *signed {
                                     WhereMetaRightDataType::SignedNum(value.to_string())
@@ -470,7 +475,6 @@ mod test {
             let mut meta = RewriteMetaData::default();
             let _ = ast[0].visit(&mut meta);
 
-            println!("meta {:#?}", meta);
             assert_eq!(
                 meta.tables
                     .values()
