f60668643d8ad1ad258d7641498d60cbf69253db
mlycore <maxwell92@126.com>
2022-12-22 00:11:40 +0800

    refactor: refactor manager layout
    Signed-off-by: mlycore <maxwell92@126.com>

---
+++ pisa-controller/cmd/pisa-controller/controller-manager/manager.go
@@ -0,0 +1,114 @@
+// Copyright 2022 SphereEx Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controllermanager
+
+import (
+	"flag"
+	"os"
+
+	"github.com/database-mesh/golang-sdk/kubernetes/client"
+	"github.com/database-mesh/pisanix/pisa-controller/cmd/pisa-controller/task"
+	"github.com/database-mesh/pisanix/pisa-controller/pkg/controllers"
+	"go.uber.org/zap/zapcore"
+	"k8s.io/api/node/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+
+	flag.Parse()
+	client.GetClient()
+}
+
+type ControllerManager struct {
+	manager.Manager
+}
+
+func (c *ControllerManager) Run() error {
+	setupLog.Info("starting operator")
+	return c.Manager.Start(ctrl.SetupSignalHandler())
+}
+
+func NewTask() task.Task {
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	var webhookPort int
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8082", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	flag.IntVar(&webhookPort, "webhook-port", 9443, "The port the webhook binds to")
+	opts := zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.RFC3339TimeEncoder,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   webhookPort,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "pisa-controller.database-mesh.io",
+		CertDir:                "/etc/pisa-controller/certs",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.VirtualDatabaseReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "VirtualDatabase")
+		os.Exit(1)
+	}
+	//TODO: Add other workload reconciler
+
+	//TODO: Add SetupWebhookWithManager
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	return &ControllerManager{
+		Manager: mgr,
+	}
+}
